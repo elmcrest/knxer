74b1ea59d451e88745cdc4996371afed1fb25645
Marius Räsener <marius@raesener.de>
2020-01-15 00:04:07 +0100

    WIP

--- KNXer/module.php
+++ KNXer/module.php
@@ -22,20 +22,25 @@ class KNXer extends IPSModule
     // Überschreibt die intere IPS_ApplyChanges($id) Funktion
     public function ApplyChanges()
     {
+        $data = json_decode(file_get_contents(__DIR__ . '/form.json'));
+        $rawData = base64_decode($this->ReadPropertyString('EtsXmlFile'));
+
+        if (@simplexml_load_string($rawData)) {
+            $data = simplexml_load_string($rawData);
+            $name = $data->GroupRange[13]->GroupRange[0]->GroupAddress[0]['Name'];
+            $this->SendDebug('KNXer', (string) $name, 0);
+        } else {
+            $this->SendDebug('KNXer', 'Errrooooor', 0);
+            $this->SetStatus(201);
+        }
+
         // Diese Zeile nicht löschen
         parent::ApplyChanges();
-
-        // $this->SetTimerInterval("CheckVersion", $this->ReadPropertyInteger("UpdateInterval") * 60 * 60 * 1000);
-        // $this->UpdateVersion();
     }
 
-    public function IPS_GetConfigurationForm()
+    // KX_ValidateEtsXmlGroupAddressExport($id)
+    public function ValidateEtsXmlGroupAddressExport()
     {
-        $data = json_decode(file_get_contents(__DIR__ . '/form.json'));
-        $rawData = substr(base64_decode($this->ReadPropertyString('EtsXmlFile')), 0, 64);
-        $xml = simplexml_load_string($rawData);
-        IPS_LogMessage('KNXer', $xml);
-        return json_encode($data);
     }
     /**
      * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
--- tests/WhateverTest.php
+++ tests/WhateverTest.php
@@ -29,4 +29,18 @@ class WhateverTest extends TestCase
         $sum = $var1 + $var2;
         $this->assertEquals(7, $sum);
     }
+
+    public function testParseXml()
+    {
+        $string = <<<XML
+        <?xml version="1.0" encoding="utf-8" standalone="yes"?><CR><LF><GroupAddress-Export xmlns="http://knx.org/xml/ga-export/01"><CR><LF>  <GroupRange Name="Outdoor" RangeStart="1" RangeEnd="2047" /><CR><LF>  <GroupRange Name="Vorderhaus/Keller" RangeStart="2048" RangeEnd="4095" /><CR><LF>  <GroupRange Name="Vorderhaus/Bunker" RangeStart="4096" RangeEnd="6143" /><CR><LF>  <GroupRange Name="Vorderhaus/EG/Bad" RangeStart="6144" RangeEnd="8191" /><CR><LF>  <GroupRange Name="Vorderhaus/EG/SchlafZi" RangeStart="8192" RangeEnd="10239" /><CR><LF>  <GroupRange Name="Vorderhaus/EG/WohnZi" RangeStart="10240" RangeEnd="12287" /><CR><LF>  <GroupRange Name="Vorderhaus/EG/WC" RangeStart="12288" RangeEnd="14335" /><CR><LF>  <GroupRange Name="Vorderhaus/EG/EssZiKü" RangeStart="14336" RangeEnd="16383" /><CR><LF>  <GroupRange Name="Vorderhaus/Flur" RangeStart="16384" RangeEnd="18431" /><CR><LF>  <GroupRange Name="Vorderhaus/OG/EssWohnZiKü" RangeStart="18432" RangeEnd="20479" /><CR><LF>  <GroupRange Name="Vorderhaus/OG/Kinderzimmer" RangeStart="20480" RangeEnd="22527" /><CR><LF>  <GroupRange Name="Vorderhaus/OG/SchlafZi" RangeStart="22528" RangeEnd="24575" /><CR><LF>  <GroupRange Name="Vorderhaus/OG/WC" RangeStart="24576" RangeEnd="26623" /><CR><LF>  <GroupRange Name="Vorderhaus/OG/Bad" RangeStart="26624" RangeEnd="28671"><CR><LF>    <GroupRange Name="Aktoren" RangeStart="26624" RangeEnd="26879"><CR><LF>      <GroupAddress Name="Stripe" Address="13/0/0" Description="light dimmed switch" DPTs="DPST-1-1" /><CR><LF>      <GroupAddress Name="Stripe" Address="13/0/1" Description="light dimmed change" DPTs="DPST-3-7" /><CR><LF>      <GroupAddress Name="Stripe" Address="13/0/2" Description="light dimmed value" DPTs="DPST-5-1" /><CR><LF>      <GroupAddress Name="Stripe" Address="13/0/3" Description="light state switch" DPTs="DPST-1-1" /><CR><LF>      <GroupAddress Name="Stripe" Address="13/0/4" Description="light state value" DPTs="DPST-5-1" /><CR><LF>      <GroupAddress Name="Spiegel" Address="13/0/5" Description="light switched switch" DPTs="DPST-1-1" /><CR><LF>      <GroupAddress Name="Spiegel" Address="13/0/6" Description="light switched lock" DPTs="DPST-1-3" /><CR><LF>      <GroupAddress Name="Spiegel" Address="13/0/7" Description="light state switch" /><CR><LF>      <GroupAddress Name="Rollo" Address="13/0/8" Description="shading drive long" /><CR><LF>      <GroupAddress Name="Rollo" Address="13/0/9" Description="shading drive short" /><CR><LF>      <GroupAddress Name="Rollo" Address="13/0/10" Description="shading state position" /><CR><LF>      <GroupAddress Name="Rollo" Address="13/0/11" Description="shading drive position" /><CR><LF>    </GroupRange><CR><LF>    <GroupRange Name="Sensoren" RangeStart="26880" RangeEnd="27135" /><CR><LF>  </GroupRange><CR><LF>  <GroupRange Name="Vorderhaus/DG" RangeStart="28672" RangeEnd="30719" /><CR><LF></GroupAddress-Export>
+        XML;
+
+        $file = file_get_contents(__DIR__ . '/fixtures/gas_test.xml', true);
+        // echo $file;
+        $cleaned = preg_replace('/<CR><LF>*/', '', $file);
+        // echo $cleaned;''
+        $xml = simplexml_load_string($cleaned);
+        $this->assertEquals('Stripe', (string) $xml->GroupRange[13]->GroupRange[0]->GroupAddress[0]['Name']);
+    }
 }
